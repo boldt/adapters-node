45832cd90b4495cbef6a011ff44db48eafa2f6c0
Dennis Boldt <boldt@itm.uni-luebeck.de>
2017-02-12 16:26:39 +0100

    Added WebRTC

--- adapters.js
+++ adapters.js
@@ -2,16 +2,25 @@
 // Adapters for node.de
 //
 
-// Crypto
 var WebCrypto = require("node-webcrypto-ossl");
-
-// TextEncoding
 var textencoding = require('text-encoding');
+var webrtc = require('./inc/webrtc');
 
 var Adapters = {
+  // WebCrypto API
+  // https://www.w3.org/TR/WebCryptoAPI/
   crypto: new WebCrypto(),
+
+  // Encoding API
+  // https://www.w3.org/TR/encoding/
   TextEncoder: textencoding.TextEncoder,
-  TextDecoder: textencoding.TextDecoder
+  TextDecoder: textencoding.TextDecoder,
+
+  // WebRTC
+  // https://www.w3.org/TR/webrtc/
+  RTCPeerConnection: webrtc.RTCPeerConnection,
+  SessionDescription: webrtc.RTCSessionDescription,
+  IceCandidate: webrtc.RTCIceCandidate
 }
 
 module.exports = Adapters;
